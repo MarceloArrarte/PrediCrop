d571b7db155121d8b7f0c5d2f27ce22094ff3608
florencia2003 <110925431+florencia2003@users.noreply.github.com>
2024-10-13 19:44:34 -0300

    Avances

--- nasaSpaceChallengeApp/src/app/app.component.ts
+++ nasaSpaceChallengeApp/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component'; 
+import { PrincipalComponent } from './principal/principal.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, PrincipalComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
